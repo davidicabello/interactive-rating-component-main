e5b458b720e7a0cd9a32f6188658d58005653e87
davidlp90 <davidivan.eda@gmail.com>
2022-11-19 19:08:36 -0300

    FALOPA
    FALOPA

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+function submit(e) {
+    const value = document.querySelector(
+        'input[name="rating"]:checked'
+    ).value;
+
+    let main = document.getElementById("main");
+    main.classList.add("hidden");
+
+    let thanks = document.getElementById("thanks");
+    thanks.classList.toggle("hidden");
+
+    document.getElementById(
+        "selection"
+    ).innerHTML = `You selected ${value} out of 5`;
+}
+
