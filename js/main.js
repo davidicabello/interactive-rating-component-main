ff553463dddfb55d96964b4a8f1b706a366a3976
davidlp90 <davidivan.eda@gmail.com>
2022-11-20 11:42:06 -0300

    final update
    final update + thankyou card

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
 
 function submit(e) {
-    const value = document.querySelector(
-        'input[name="rating"]:checked'
-    ).value;
-
+    const value = document.querySelector('input[name="rating"]:checked').value;
+    document.getElementById("selection").innerHTML = `You selected ${value} out of 5`;
     let main = document.getElementById("main");
     main.classList.add("hidden");
 
     let thanks = document.getElementById("thanks");
     thanks.classList.toggle("hidden");
 
-    document.getElementById(
-        "selection"
-    ).innerHTML = `You selected ${value} out of 5`;
+    
 }
 
